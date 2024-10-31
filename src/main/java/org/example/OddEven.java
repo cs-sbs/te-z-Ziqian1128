5f1925959557102b6a4bdb4775435d5bf99ef2b3
Ziqian1128 <2873787690@qq.com>
2024-10-31 19:10:46 +0800

    Update OddEven.java

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -2,24 +2,24 @@ package org.example;
 
 import java.util.Scanner;
 
-public class OddEven{
+public class OddEven {
     public static void main(String[] args) {
         int number;
         int countOdd = 0;
-        int countOdd = 0;
-        Scanner in = new Scanner(System.in);
+        int countEven = 0;
+        Scanner in = new Scanner(System.in); 
         do{
-           number = in.nextInt();
-            if (number != -1) 
+            number = in.nextInt();
+            if(number != -1)
             {
-            if (number % 2 == 0) {
-                counteven += 1;
-            } else {
-                counteven += 1;
+                if (number % 2 == 0) {
+                    countEven += 1;
+                } else {
+                    countOdd += 1;
+                }
             }
-        }
-        }while(number ! = -1);
-        System.out.print(countOdd+"");
+        }while(number != -1); 
+        System.out.print(countOdd+" ");
         System.out.println(countEven);
     }
 }
