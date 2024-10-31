a5f8fa6137c38280a3407652e304ba76b96d3a1a
Ziqian1128 <2873787690@qq.com>
2024-10-31 19:27:10 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -8,7 +8,17 @@ public class NumberFeature {
         int result = 0; // 输出结果
         Scanner in = new Scanner(System.in);
         inputNumber = in.nextInt();
-        // 你的代码逻辑
+        int digitIndex = 1;  
+        while (inputNumber > 0) {  
+            int digit = inputNumber % 10;
+            if ((digit % 2 == 0 && digitIndex % 2 == 0) || (digit % 2!= 0 && digitIndex % 2!= 0)) {  
+                result = result * 2 + 1;  
+            } else {
+                result *= 2;  
+            }
+            inputNumber /= 10;
+            digitIndex++;  
+        }
 
         System.out.println(result);
     }
