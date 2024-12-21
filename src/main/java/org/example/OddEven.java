2f6eef1e27e938197669197e386fdae3d2197b28
Ziqian1128 <2873787690@qq.com>
2024-12-21 15:46:31 +0800

    Update OddEven.java

--- src/main/java/org/example/OddEven.java
+++ src/main/java/org/example/OddEven.java
@@ -7,7 +7,7 @@ public class OddEven {
         int number;
         int countOdd = 0;
         int countEven = 0;
-        Scanner in = new Scanner(System.in); 
+        Scanner in = new Scanner(System.in); // 数字用空格或换行隔开，每次读入一个，直到输入‘-1’
         do{
             number = in.nextInt();
             if(number != -1)
@@ -18,7 +18,7 @@ public class OddEven {
                     countOdd += 1;
                 }
             }
-        }while(number != -1); 
+        }while(number != -1);  // 理解do while和while循环的区别
         System.out.print(countOdd+" ");
         System.out.println(countEven);
     }
